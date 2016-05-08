cdc4b26ad11e7899ae0c823e9806a921a3b78d59
djun100 <djun100@qq.com>
2016-05-09 00:03:06 +0800

    增加正则处理文本工具类

---
+++ src/main/java/com/cy/util/UtilStringFactory.java
@@ -0,0 +1,61 @@
+package com.cy.util;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by cy on 2016/5/8.
+ */
+public class UtilStringFactory {
+    /**<pre>按正则格式提取原文中的有用信息并按正则格式返回
+     * eg:资金账户管理（6）http://demo.abc.com/APP/app_fundAccountManage.aspx
+     * 账户相关与系统设置（13）http://demo.shbgz.com/APP/app_accountSystemSettings.aspx
+     * @param originLine 原文任意行信息
+     * @param regexExtract 按该正则从关键行提取关键信息
+     *                     positon 0:关键行，position 1：关键信息1，position 2：关键信息2...
+     * @param  formatOut 按照关键信息提取顺序，将提取的行信息按格式化输出
+     *                   eg:public static String URL_%s="%s";
+     *
+     *
+     *
+     * <pre>todo 给出示例关键信息，反推正则表达式，列出相似性匹配结果
+     *
+     * */
+    public static String processFactory(String originLine, List<String>regexExtract, String formatOut){
+        // TODO: 2016/5/8 按正则提取有用信息
+        // TODO: 2016/5/8 判断该行是否为匹配行，是→继续
+        ArrayList<String> keys=new ArrayList<>();
+        Pattern p = Pattern.compile(regexExtract.get(0));
+        Matcher m = p.matcher(originLine); // 获取 matcher 对象
+        if (m.find()){
+            for (int i = 1; i < regexExtract.size(); i++) {
+                p=Pattern.compile(regexExtract.get(i));
+                m=p.matcher(originLine);
+                if (m.find()){
+                    String keyInfo=m.group();
+                    System.out.println("关键信息："+keyInfo);
+                    keys.add(keyInfo);
+                }else {
+                    throw new RuntimeException("未在关键行匹配到关键信息");
+                }
+            }
+        }else {
+            return null;
+        }
+        String result=String.format(formatOut,keys);
+        return result;
+    }
+
+    public static void main(String[] args){
+        String orignLine="资金账户管理（6）http://demo.abc.com/APP/app_fundAccountManage.aspx dd";
+        ArrayList<String> regex=new ArrayList<String>();
+        regex.add("(http://){1}");
+        regex.add("(http://){1}[a-zA-Z0-9_/.]+(.aspx){1}");
+        String formatOut="public static String URL_=\"%s\"";
+//        String formatOut="public static String URL_%s=\"%s\"";
+        String result=processFactory(orignLine,regex,formatOut);
+        System.out.println(result);
+    }
+}
