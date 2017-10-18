c44489ed3fae3df925c03d6302d57f229f76027c
wangxuechao <djun100@qq.com>
2017-10-18 11:07:00 +0800

    update

--- src/main/java/com/cy/data/UString.java
+++ src/main/java/com/cy/data/UString.java
@@ -240,4 +240,8 @@ public class UString {
 		}
 		return "";
 	}
+
+	public static String repeat(String toRepeat,int times){
+		return new String(new char[times]).replace("\0", toRepeat);
+	}
 }
---
+++ src/test/java/UStringTest.java
@@ -0,0 +1,9 @@
+import com.cy.data.UString;
+import org.junit.Test;
+
+public class UStringTest {
+    @Test
+    public void test(){
+        System.out.println(UString.repeat("哈哈",5));
+    }
+}
